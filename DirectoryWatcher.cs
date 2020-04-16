f983bf216dc3f0cad14a07e85403599d2878c22d
Microbubu <microbubu@live.com>
2020-04-17 03:17:17 +0800

    Revert "Add TopShelf."
    This reverts commit 34b01039eaff2dc2c3ad26e9fb7da2a64156e1db.

--- DirectoryWatcher.cs
+++ DirectoryWatcher.cs
@@ -1,4 +1,7 @@
-﻿using System.IO;
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
 
 namespace FolderLinker
 {
@@ -14,10 +17,6 @@ namespace FolderLinker
         {
             srcDir = src;
             dstDir = dst;
-        }
-
-        public void StartWatch()
-        {
             watcher = new FileSystemWatcher(srcDir);
             watcher.NotifyFilter = NotifyFilters.DirectoryName;
             watcher.EnableRaisingEvents = true;
@@ -47,7 +46,6 @@ namespace FolderLinker
             if (Directory.Exists(dstOldName))
             {
                 Directory.Move(dstOldName, dstNewName);
-                Service.Log.WriteLine($"{dstOldName} ⋙ {dstNewName}", LogType.Rename);
             }
         }
 
@@ -66,10 +64,7 @@ namespace FolderLinker
         {
             string dstPath = Path.Combine(dstDir, e.Name);
             if (Directory.Exists(dstPath))
-            {
                 Directory.Delete(dstPath);
-                Service.Log.WriteLine($"{dstPath}", LogType.Delete);
-            }
         }
 
         private void CreatedHandler(object sender, FileSystemEventArgs e)
--- Linker.cs
+++ Linker.cs
@@ -1,24 +1,23 @@
-﻿using System.Management.Automation;
+using System;
+using System.Management.Automation;
+using System.Linq;
 
-namespace FolderLinker
-{
-    public class Linker
+public class Linker{
+    private string srcDir;
+    private string dstDir;
+    public Linker(string src, string dst)
     {
-        private string srcDir;
-        private string dstDir;
-        public Linker(string src, string dst)
-        {
-            this.srcDir = src;
-            this.dstDir = dst;
-        }
+        this.srcDir=src;
+        this.dstDir=dst;
+    }
 
-        public void Link()
-        {
-            string script = "new-item -itemtype Junction -path " + $"\"{srcDir}\"" +
-                            " -target " + $"\"{dstDir}\"";
-            PowerShell ps = PowerShell.Create().AddScript(script);
-            ps.Invoke();
-            Service.Log.WriteLine($"{dstDir} ⋙ {srcDir}", LogType.Link);
-        }
+    public void Link()
+    {
+        Console.WriteLine($"Linking {dstDir} to {srcDir}...");
+        string script = "new-item -itemtype Junction -path " + $"\"{srcDir}\"" +
+                        " -target " + $"\"{dstDir}\"";
+        PowerShell ps = PowerShell.Create().AddScript(script);
+        ps.Invoke();
+        Console.WriteLine("Done!");
     }
 }
